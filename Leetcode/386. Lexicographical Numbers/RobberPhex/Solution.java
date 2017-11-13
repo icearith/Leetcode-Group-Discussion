aae1e41504bf58e6ba1e7edcac48a74c1e81b113
Robert Lu <robberphex@gmail.com>
2017-11-13 14:23:38 +0800

    solve 386 with 128ms

---
+++ Leetcode/386. Lexicographical Numbers/RobberPhex/Solution.java
@@ -0,0 +1,31 @@
+import java.util.LinkedList;
+import java.util.List;
+
+class Solution {
+
+    public List<Integer> lexicalOrder(int max) {
+        List<Integer> list = new LinkedList<Integer>();
+        int cur = 1;
+        for (int i = 0; i < max; i++) {
+            list.add(cur);
+            if (cur * 10 <= max)
+                cur *= 10;
+            else {
+                if (cur >= max)
+                    cur /= 10;
+                cur++;
+                while (cur % 10 == 0)
+                    cur /= 10;
+            }
+        }
+
+        return list;
+    }
+
+    public static void main(String[] args) {
+        List<Integer> res = new Solution().lexicalOrder(234);
+        for (int e : res) {
+            System.out.println(e);
+        }
+    }
+}
