b22fffa93fe2eca5b4a8634463976478d51e02c4
Robert Lu <robberphex@gmail.com>
2017-11-28 01:30:30 +0800

    simple version of 21

--- Leetcode/21. Merge Two Sorted Lists/Robberphex/Solution.java
+++ Leetcode/21. Merge Two Sorted Lists/Robberphex/Solution.java
@@ -1,22 +1,16 @@
 class Solution {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        ListNode tmpHead = new ListNode(0);
-        ListNode cur1 = l1, cur2 = l2, cur = tmpHead;
-        while (cur1 != null && cur2 != null) {
-            if (cur1.val < cur2.val) {
-                cur.next = cur1;
-                cur1 = cur1.next;
-            } else {
-                cur.next = cur2;
-                cur2 = cur2.next;
-            }
-            cur = cur.next;
-        }
-        if (cur1 != null) {
-            cur.next = cur1;
+        if (l1 == null)
+            return l2;
+        if (l2 == null)
+            return l1;
+
+        if (l1.val < l2.val) {
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
         } else {
-            cur.next = cur2;
+            l2.next = mergeTwoLists(l1, l2.next);
+            return l2;
         }
-        return tmpHead.next;
     }
 }
