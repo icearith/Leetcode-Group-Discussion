6885a2005343444ecb1c7ec028b4d57a836c3f3d
Robert Lu <robberphex@gmail.com>
2017-11-29 12:59:05 +0800

    solve 58 with 5ms

--- Leetcode/58. Length of Last Word/Robberphex/Solution.java
+++ Leetcode/58. Length of Last Word/Robberphex/Solution.java
@@ -1,25 +1,7 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        if (s.length() == 0)
-            return 0;
-        char[] chars = s.toCharArray();
-        int i = chars.length - 1;
-        while (i >= 0 && chars[i] == ' ')
-            i--;
-        if (i < 0)
-            return 0;
-        int end = i;
-        while (i >= 0) {
-            if (chars[i] == ' ')
-                break;
-            i--;
-        }
-        return end - i;
-    }
-
-    public static void main(String[] args) {
-        Solution obj = new Solution();
-        int ret = obj.lengthOfLastWord("");
-        System.out.println(ret);
+        s = s.trim();
+        int lastIndex = s.lastIndexOf(' ') + 1;
+        return s.length() - lastIndex;
     }
 }
