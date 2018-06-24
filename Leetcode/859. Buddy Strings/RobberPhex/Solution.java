49664fe66c2a14821a0f849f1524416cec371c7a
Robert Lu <robberphex@gmail.com>
2018-06-24 12:59:04 +0800

    format

--- Leetcode/859. Buddy Strings/RobberPhex/Solution.java
+++ Leetcode/859. Buddy Strings/RobberPhex/Solution.java
@@ -2,9 +2,7 @@ class Solution {
     public boolean buddyStrings(String A, String B) {
         if (A.length() != B.length())
             return false;
-        if (A.length() == 0)
-            return false;
-        int f = -1, s = -1;
+        int firstDiffPos = -1, secondDiffPos = -1;
         int stat[] = new int[300];
         boolean two = false;
         for (int i = 0; i < A.length(); i++) {
@@ -13,24 +11,24 @@ class Solution {
                 two = true;
             }
             if (A.charAt(i) != B.charAt(i)) {
-                f = i;
+                firstDiffPos = i;
                 break;
             }
         }
-        if (f == -1)
+        if (firstDiffPos == -1)
             return two;
-        for (int i = f + 1; i < A.length(); i++) {
+        for (int i = firstDiffPos + 1; i < A.length(); i++) {
             stat[A.charAt(i)]++;
             if (A.charAt(i) != B.charAt(i)) {
-                s = i;
+                secondDiffPos = i;
                 break;
             }
         }
-        if (s == -1)
+        if (secondDiffPos == -1)
             return false;
-        if (A.charAt(f) != B.charAt(s) || A.charAt(s) != B.charAt(f))
+        if (A.charAt(firstDiffPos) != B.charAt(secondDiffPos) || A.charAt(secondDiffPos) != B.charAt(firstDiffPos))
             return false;
-        for (int i = s + 1; i < A.length(); i++) {
+        for (int i = secondDiffPos + 1; i < A.length(); i++) {
             stat[A.charAt(i)]++;
             if (A.charAt(i) != B.charAt(i)) {
                 return false;
@@ -40,6 +38,6 @@ class Solution {
     }
 
     public static void main(String[] args) {
-        new Solution().buddyStrings("de", "ed");
+        new Solution().buddyStrings("", "");
     }
 }
