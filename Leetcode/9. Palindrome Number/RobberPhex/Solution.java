8549b9403b0af96f46c737703261d2c4b57fc24c
Robert Lu <robberphex@gmail.com>
2018-07-08 19:14:28 +0800

    solve 9, better

--- Leetcode/9. Palindrome Number/RobberPhex/Solution.java
+++ Leetcode/9. Palindrome Number/RobberPhex/Solution.java
@@ -1,21 +1,20 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        if(x<0)
+        if (x < 0 || (x % 10 == 0 && x != 0))
             return false;
-        int tmp = x;
         int pair = 0;
-        while (tmp > 0) {
-            pair *=10;
-            pair += (tmp % 10);
-            tmp /= 10;
+        while (x > pair) {
+            pair *= 10;
+            pair += (x % 10);
+            x /= 10;
         }
-        return x == pair;
+        return x == pair || x == pair / 10;
     }
 
     public static void main(String[] args) {
         boolean res = new Solution().isPalindrome(121);
         System.out.println(res);
-        res = new Solution().isPalindrome(-121);
+        res = new Solution().isPalindrome(1221);
         System.out.println(res);
         res = new Solution().isPalindrome(10);
         System.out.println(res);
