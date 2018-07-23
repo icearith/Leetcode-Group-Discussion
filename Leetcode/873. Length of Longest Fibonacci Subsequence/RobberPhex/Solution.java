af152514b4327907ac1bd2eb975e745deac47fa8
Robert Lu <robberphex@gmail.com>
2018-07-23 23:41:06 +0800

    use dp

--- Leetcode/873. Length of Longest Fibonacci Subsequence/RobberPhex/Solution.java
+++ Leetcode/873. Length of Longest Fibonacci Subsequence/RobberPhex/Solution.java
@@ -1,37 +1,38 @@
-import java.util.HashSet;
-import java.util.Set;
+import java.util.HashMap;
+import java.util.Map;
 
 class Solution {
     public int lenLongestFibSubseq(int[] A) {
         int maxLength = 0;
-        Set<Integer> numbers = new HashSet<>(A.length);
+        Map<Integer, Integer> index = new HashMap<>(A.length);
         for (int i = 0; i < A.length; i++)
-            numbers.add(A[i]);
+            index.put(A[i], i);
 
-        for (int i = 0; i < A.length - 1; i++) {
-            for (int j = i + 1; j < A.length; j++) {
-                int prev1 = A[i];
-                int prev2 = A[j];
-                int length = 2;
-                while (numbers.contains(prev1 + prev2)) {
-                    length++;
-                    int tmp = prev1;
-                    prev1 = prev2;
-                    prev2 += tmp;
+        HashMap<Integer, Integer> longest = new HashMap<>();
+        for (int j = 0; j < A.length - 1; j++) {
+            for (int k = j + 1; k < A.length; k++) {
+                int i = index.getOrDefault(A[k] - A[j], -1);
+                if (i >= 0 && i < j) {
+                    int curLength = longest.getOrDefault(i * A.length + j, 2) + 1;
+                    longest.put(j * A.length + k, curLength);
+                    maxLength = Math.max(maxLength, curLength);
                 }
-                if (length > 2)
-                    maxLength = Math.max(maxLength, length);
             }
         }
-        return maxLength;
+        if (maxLength > 2)
+            return maxLength;
+        return 0;
     }
 
     public static void main(String[] args) {
         int res;
+        // 5
         res = new Solution().lenLongestFibSubseq(new int[]{1, 2, 3, 4, 5, 6, 7, 8});
         System.out.println(res);
+        // 3
         res = new Solution().lenLongestFibSubseq(new int[]{1, 3, 7, 11, 12, 14, 18});
         System.out.println(res);
+        // 5
         res = new Solution().lenLongestFibSubseq(new int[]{2, 4, 7, 8, 9, 10, 14, 15, 18, 23, 32, 50});
         System.out.println(res);
     }
