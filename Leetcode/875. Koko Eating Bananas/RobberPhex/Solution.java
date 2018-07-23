cb4a783013094c04c391ea541abf3c662c9784cf
Robert Lu <robberphex@gmail.com>
2018-07-23 18:19:22 +0800

    faster

--- Leetcode/875. Koko Eating Bananas/RobberPhex/Solution.java
+++ Leetcode/875. Koko Eating Bananas/RobberPhex/Solution.java
@@ -1,43 +1,30 @@
-/*
- * 这个实际上是后来才抄出来的二分法
- * 但是居然比原来慢，所以我只能将原来的放在后面了。。 
- */
 class Solution {
     public int minEatingSpeed(int[] piles, int H) {
         long sum = 0;
-        int max = 0;
-        for (int i = 0; i < piles.length; i++) {
+        for (int i = 0; i < piles.length; i++)
             sum += piles[i];
-            max = Math.max(max, piles[i]);
-        }
-        int min = (int) ((sum + H - 1) / H);
-        while (min < max) {
-            int mid = (min + max) / 2;
-            if (possible(piles, mid, H))
-                max = mid;
-            else
-                min = mid + 1;
-        }
-        return max;
-    }
+        int min = (int) (sum / H);
+        if (1L * min * H < sum)
+            min++;
 
-    private boolean possible(int[] piles, int speed, int H) {
-        int hours = 0;
-        for (int i = 0; i < piles.length; i++) {
-            hours += (piles[i] + speed - 1) / speed;
+        while (true) {
+            int hours = 0;
+            for (int i = 0; i < piles.length; i++) {
+                hours += (piles[i] + min - 1) / min;
+            }
+            if (hours <= H)
+                return min;
+            min++;
         }
-        if (hours <= H)
-            return true;
-        return false;
     }
 
     public static void main(String[] args) {
         int res;
         res = new Solution().minEatingSpeed(new int[]{3, 6, 7, 11}, 8);
         System.out.println(res);
-        res = new Solution().minEatingSpeed(new int[]{30, 11, 23, 4, 20}, 5);
+        res = new Solution().minEatingSpeed(new int[]{30,11,23,4,20}, 5);
         System.out.println(res);
-        res = new Solution().minEatingSpeed(new int[]{30, 11, 23, 4, 20}, 6);
+        res = new Solution().minEatingSpeed(new int[]{30,11,23,4,20}, 6);
         System.out.println(res);
     }
 }
