60371ee58230ecee234af5e30baa36c8891b23e6
Robert Lu <robberphex@gmail.com>
2018-07-26 03:00:25 +0800

    solve 35, brutal but works

---
+++ Leetcode/35. Search Insert Position/RobberPhex/Solution.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int min = 0, max = nums.length - 1;
+        if (target > nums[nums.length - 1])
+            return nums.length;
+        while (min < max) {
+            int mid = (min + max) / 2;
+            if (nums[mid] < target) {
+                min = mid + 1;
+            } else {
+                max = mid;
+            }
+        }
+        return min;
+    }
+
+    public static void main(String[] args) {
+        int res;
+
+        res = new Solution().searchInsert(new int[]{1, 3, 5, 6}, 5);
+        System.out.println(res);
+
+        res = new Solution().searchInsert(new int[]{1, 3, 5, 6}, 2);
+        System.out.println(res);
+
+        res = new Solution().searchInsert(new int[]{1, 3, 5, 6}, 7);
+        System.out.println(res);
+
+        res = new Solution().searchInsert(new int[]{1, 3, 5, 6}, 0);
+        System.out.println(res);
+    }
+}
