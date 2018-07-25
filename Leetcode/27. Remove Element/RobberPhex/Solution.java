2329ccdce2dbf7261ddbcf68720d9e9239a91d26
Robert Lu <robberphex@gmail.com>
2018-07-26 02:37:29 +0800

    simplified

--- Leetcode/27. Remove Element/RobberPhex/Solution.java
+++ Leetcode/27. Remove Element/RobberPhex/Solution.java
@@ -1,14 +1,7 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
         int index = 0;
-        int i;
-        for (i = 0; i < nums.length; i++) {
-            if (nums[i] != val) {
-                nums[index] = nums[i];
-                index++;
-            }
-        }
-        for (; i < nums.length; i++) {
+        for (int i=0; i < nums.length; i++) {
             if (nums[i] != val) {
                 nums[index] = nums[i];
                 index++;
