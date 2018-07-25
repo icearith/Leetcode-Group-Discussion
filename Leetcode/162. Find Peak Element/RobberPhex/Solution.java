581b21cb84a560d72dcd315fa676137b0cfb8417
Robert Lu <robberphex@gmail.com>
2018-07-25 17:00:19 +0800

    simplified

--- Leetcode/162. Find Peak Element/RobberPhex/Solution.java
+++ Leetcode/162. Find Peak Element/RobberPhex/Solution.java
@@ -1,16 +1,14 @@
 class Solution {
     public int findPeakElement(int[] A) {
-        for (int i = 0; i < A.length; i++)
-            if ((i < 1 || A[i - 1] < A[i]) && (i + 1 >= A.length || A[i] > A[i + 1]))
+        for (int i = 0; i < A.length - 1; i++)
+            if (A[i] > A[i + 1])
                 return i;
-        return 0;
+        return A.length - 1;
     }
 
     public static void main(String[] args) {
         int res;
-        res = new Solution().findPeakElement(new int[]{1, 2, 3, 1});
-        System.out.println(res);
-        res = new Solution().findPeakElement(new int[]{1, 2, 1, 3, 5, 6, 4});
+        res = new Solution().findPeakElement(new int[]{2, 1});
         System.out.println(res);
     }
 }
