37ce2b9e00d9fe73f1c0011be25f0b4b3b3b8f15
Robert Lu <robberphex@gmail.com>
2018-07-27 18:22:43 +0800

    better

--- Leetcode/101. Symmetric Tree/RobberPhex/Solution.java
+++ Leetcode/101. Symmetric Tree/RobberPhex/Solution.java
@@ -1,31 +1,23 @@
-import java.util.LinkedList;
-import java.util.Queue;
-
 class Solution {
-    private Queue<TreeNode> queue = new LinkedList<>();
-
     public boolean isSymmetric(TreeNode root) {
-        if (root == null)
+        if(root==null)
             return true;
-        queue.add(root.left);
-        queue.add(root.right);
-        while (!queue.isEmpty()) {
-            TreeNode t1 = queue.poll();
-            TreeNode t2 = queue.poll();
-            if (t1 == null && t2 == null)
-                continue;
-            if (t1 == null || t2 == null)
-                return false;
-            if (t1.val != t2.val)
-                return false;
-            queue.add(t1.left);
-            queue.add(t2.right);
-            queue.add(t1.right);
-            queue.add(t2.left);
-        }
-        return true;
+        return isSymmetric(root.left, root.right);
     }
 
+    private boolean isSymmetric(TreeNode left, TreeNode right) {
+        if (left == null && right == null)
+            return true;
+        if (left == null || right == null)
+            return false;
+        if (left.val != right.val)
+            return false;
+        if (!isSymmetric(left.right, right.left))
+            return false;
+        if (!isSymmetric(left.left, right.right))
+            return false;
+        return true;
+    }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
