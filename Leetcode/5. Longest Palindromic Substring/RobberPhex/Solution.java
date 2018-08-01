22d60de9a982dd408d4ea46ac66a7447106adc93
Robert Lu <robberphex@gmail.com>
2018-08-01 17:55:24 +0800

    Manacher's Algorithm with StringBuilder

--- Leetcode/5. Longest Palindromic Substring/RobberPhex/Solution.java
+++ Leetcode/5. Longest Palindromic Substring/RobberPhex/Solution.java
@@ -1,12 +1,17 @@
 class Solution {
     public String longestPalindrome(String s) {
-        MyString str = new MyString(s);
-        int[] p = new int[str.length()];
+        StringBuilder sb = new StringBuilder();
+        sb.append(Character.MAX_VALUE);
+        for(int i=0;i<s.length();i++) {
+            sb.append(s.charAt(i));
+            sb.append(Character.MAX_VALUE);
+        }
+        int[] p = new int[sb.length()];
         int id = 0, mx = 0;
         int sIndex = 0, eIndex = 0;
-        for (int i = 0; i < str.length(); i++) {
+        for (int i = 0; i < sb.length(); i++) {
             p[i] = mx > i ? Math.min(mx - i, p[id * 2 - i]) : 1;
-            while (i + p[i] < str.length() && i - p[i] >= 0 && str.charAt(i + p[i]) == str.charAt(i - p[i]))
+            while (i + p[i] < sb.length() && i - p[i] >= 0 && sb.charAt(i + p[i]) == sb.charAt(i - p[i]))
                 p[i]++;
             if (p[i] > eIndex - sIndex) {
                 if (i % 2 == 0) {
@@ -30,25 +35,5 @@ class Solution {
         Solution solution = new Solution();
         String res = solution.longestPalindrome("");
         System.out.println(res);
-    }
-}
-
-class MyString {
-    private String s;
-    private int len;
-
-    MyString(String s) {
-        this.s = s;
-        len = s.length() * 2 + 1;
-    }
-
-    public int length() {
-        return len;
-    }
-
-    public char charAt(int index) {
-        if ((index + 1) % 2 == 0)
-            return s.charAt((index + 1) / 2 - 1);
-        return Character.MAX_VALUE;
     }
 }
