9036a5594da089c05b828347a253963ea8f616ba
Robert Lu <robberphex@gmail.com>
2018-08-01 16:18:26 +0800

    better

--- Leetcode/4. Median of Two Sorted Arrays/RobberPhex/Solution.java
+++ Leetcode/4. Median of Two Sorted Arrays/RobberPhex/Solution.java
@@ -1,27 +1,38 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int[] merged = new int[nums1.length + nums2.length];
+        if (nums1.length < nums2.length) {
+            int[] tmp = nums1;
+            nums1 = nums2;
+            nums2 = tmp;
+        }
+        int low = 0, high = nums2.length * 2;
+        while (low <= high) {
+            int mid2 = (low + high) / 2;
+            int mid1 = nums1.length + nums2.length - mid2;
 
-        int l1 = 0, l2 = 0;
-        for (int i = 0; i < merged.length; i++) {
-            if (l1 < nums1.length && (l2 >= nums2.length || nums1[l1] < nums2[l2])) {
-                merged[i] = nums1[l1];
-                l1++;
+            int L1 = mid1 <= 0 ? Integer.MIN_VALUE : nums1[(mid1 - 1) / 2];
+            int L2 = mid2 <= 0 ? Integer.MIN_VALUE : nums2[(mid2 - 1) / 2];
+            int R1 = mid1 >= nums1.length * 2 ? Integer.MAX_VALUE : nums1[mid1 / 2];
+            int R2 = mid2 >= nums2.length * 2 ? Integer.MAX_VALUE : nums2[mid2 / 2];
+            // we need L1<R2 and L2<R1
+            if (L1 > R2) {
+                // we need mid2 to bigger
+                low = mid2 + 1;
+            } else if (L2 > R1) {
+                high = mid2 - 1;
             } else {
-                merged[i] = nums2[l2];
-                l2++;
+                return (0.0 + Math.max(L1, L2) + Math.min(R1, R2)) / 2;
             }
         }
-        if (merged.length % 2 == 1) {
-            return merged[merged.length / 2];
-        } else {
-            return (merged[merged.length / 2] + merged[(merged.length - 1) / 2]) / 2.0;
-        }
+        throw new RuntimeException();
     }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.findMedianSortedArrays(new int[]{1}, new int[]{2, 3}));
-        System.out.println(solution.findMedianSortedArrays(new int[]{1, 2}, new int[]{3, 4}));
+        double res = solution.findMedianSortedArrays(
+                new int[]{1, 2},
+                new int[]{3, 4}
+        );
+        System.out.println(res);
     }
 }
