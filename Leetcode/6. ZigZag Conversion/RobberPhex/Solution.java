8b5aa39af3fb3c6182c0847963e6b54497a8359b
Robert Lu <robberphex@gmail.com>
2018-08-01 18:30:03 +0800

    better performance

--- Leetcode/6. ZigZag Conversion/RobberPhex/Solution.java
+++ Leetcode/6. ZigZag Conversion/RobberPhex/Solution.java
@@ -1,32 +1,27 @@
 class Solution {
     public String convert(String s, int numRows) {
+        if (numRows <= 1 || s.length() < numRows)
+            return s;
         int totalStep = 2 * (numRows - 1);
-        if (numRows == 1)
-            totalStep = 1;
+        char[] retChars = new char[s.length()];
+        int index = 0;
 
-        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < numRows; i++) {
-            int index = i;
-            while (index < s.length()) {
-                sb.append(s.charAt(index));
-                index += (totalStep - i * 2);
-                if (i == 0)
-                    continue;
-                if (i == numRows - 1) {
-                    index += totalStep;
-                } else if (index < s.length()) {
-                    sb.append(s.charAt(index));
-                    index += i * 2;
+            int step = totalStep - 2 * i;
+            for (int j = i; j < s.length(); j += totalStep) {
+                retChars[index++] = s.charAt(j);
+                if (step > 0 && step < totalStep && j + step < s.length()) {
+                    retChars[index++] = s.charAt(j + step);
                 }
             }
         }
-        return sb.toString();
+        return new String(retChars);
     }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
         String res;
-        res= solution.convert("PAYPALISHIRING", 4);
+        res = solution.convert("PAYPALISHIRING", 4);
         System.out.println(res.equals("PINALSIGYAHRPI"));
         res = solution.convert("A", 1);
         System.out.println(res);
