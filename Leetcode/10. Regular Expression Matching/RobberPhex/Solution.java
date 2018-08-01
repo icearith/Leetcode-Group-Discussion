85b6fa9a4d5bf252a7cd6b648359c551c53f795d
Robert Lu <robberphex@gmail.com>
2018-08-02 02:51:31 +0800

    simple solution

--- Leetcode/10. Regular Expression Matching/RobberPhex/Solution.java
+++ Leetcode/10. Regular Expression Matching/RobberPhex/Solution.java
@@ -1,53 +1,19 @@
-enum Result {
-    TRUE, FALSE
-}
-
 class Solution {
-    private Result[][] results;
-
     public boolean isMatch(String s, String p) {
-        results = new Result[s.length() + 1][p.length() + 1];
-        return isMatch(s, p, 0, 0);
-    }
-
-    private boolean isMatch(String s, String p, int indexS, int indexP) {
-        if (results[indexS][indexP] != null) {
-            return results[indexS][indexP] == Result.TRUE;
-        }
-        while (indexP < p.length()) {
-            if (indexP + 1 < p.length() && p.charAt(indexP + 1) == '*') {
-                // 处理带星号的情况
-                for (; indexS < s.length(); indexS++) {
-                    if (isMatch(s, p, indexS, indexP + 2)) {
-                        results[indexS][indexP + 2] = Result.TRUE;
-                        return true;
-                    } else if (p.charAt(indexP) != '.' && p.charAt(indexP) != s.charAt(indexS)) {
-                        results[indexS][indexP] = Result.FALSE;
-                        return false;
-                    }
-                    results[indexS][indexP] = Result.FALSE;
+        boolean[][] dp = new boolean[s.length() + 1][p.length() + 1];
+        dp[s.length()][p.length()] = true;
+        for (int sIndex = s.length(); sIndex >= 0; sIndex--) {
+            for (int pIndex = p.length() - 1; pIndex >= 0; pIndex--) {
+                boolean first_match = sIndex < s.length() &&
+                        (s.charAt(sIndex) == p.charAt(pIndex) || p.charAt(pIndex) == '.');
+                if (pIndex + 1 < p.length() && p.charAt(pIndex + 1) == '*') {
+                    dp[sIndex][pIndex] = dp[sIndex][pIndex + 2] || first_match && dp[sIndex + 1][pIndex];
+                } else {
+                    dp[sIndex][pIndex] = first_match && dp[sIndex + 1][pIndex + 1];
                 }
-                results[indexS][indexP] = Result.TRUE;
-                indexP += 2;
-                results[indexS][indexP] = Result.TRUE;
-            } else if (p.charAt(indexP) != '.' && (indexS >= s.length() || s.charAt(indexS) != p.charAt(indexP))) {
-                results[indexS][indexP] = Result.FALSE;
-                return false;
-            } else {
-                if (indexS >= s.length()) {
-                    results[indexS][indexP] = Result.FALSE;
-                    return false;
-                }
-                indexP++;
-                indexS++;
             }
         }
-        if (indexS < s.length()) {
-            results[indexS][indexP] = Result.FALSE;
-            return false;
-        }
-        results[indexS][indexP] = Result.TRUE;
-        return true;
+        return dp[0][0];
     }
 
     public static void main(String[] args) {
