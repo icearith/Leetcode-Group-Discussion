771dbb3f1646c69bd81ea2042db5007e78a424c8
Robert Lu <robberphex@gmail.com>
2018-08-03 00:47:59 +0800

    better

--- Leetcode/18. 4Sum/RobberPhex/Solution.java
+++ Leetcode/18. 4Sum/RobberPhex/Solution.java
@@ -4,13 +4,23 @@ import java.util.List;
 
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
+        if (nums.length < 4)
+            return new LinkedList<>();
         Arrays.sort(nums);
         List<List<Integer>> results = new LinkedList<>();
-        for (int i = 0; i < nums.length; i++) {
+        for (int i = 0; i < nums.length - 3; i++) {
             if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+            if (nums[i] + nums[i + 1] + nums[i + 2] + nums[i + 3] > target)
+                break;
+            if (nums[i] + nums[nums.length - 1] + nums[nums.length - 2] + nums[nums.length - 3] < target)
                 continue;
             for (int j = i + 1; j < nums.length - 2; j++) {
                 if (j > i + 1 && nums[j] == nums[j - 1])
+                    continue;
+                if (nums[i] + nums[j] + nums[j + 1] + nums[j + 2] > target)
+                    break;
+                if (nums[i] + nums[j] + nums[nums.length - 1] + nums[nums.length - 2] < target)
                     continue;
                 twoSum(nums, results, j + 1, target, new Integer[]{nums[i], nums[j], 0, 0});
             }
@@ -24,12 +34,8 @@ class Solution {
             int sum = pResult[0] + pResult[1] + nums[start] + nums[end];
             if (sum < target) {
                 start++;
-                while (start < end && nums[start] == nums[start - 1])
-                    start++;
             } else if (sum > target) {
                 end--;
-                while (start < end && nums[end] == nums[end + 1])
-                    end--;
             } else {
                 pResult[2] = nums[start];
                 pResult[3] = nums[end];
